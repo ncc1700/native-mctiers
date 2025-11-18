37126d45182879077152b464a31e346834bcbbbb
ncc1700 <nearestcommit@gmail.com>
2025-11-17 19:37:00 -0600

    s

--- src/extern/naett/naett.c
+++ src/extern/naett/naett.c
@@ -5,7 +5,7 @@
 #ifndef NAETT_INTERNAL_H
 #define NAETT_INTERNAL_H
 
-#ifdef _MSC_VER
+#ifdef _WIN32
     #define strcasecmp _stricmp
     #undef strdup
     #define strdup _strdup
@@ -44,17 +44,6 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
- 
-
-char* strdup(const char* string) {
-    size_t size = strlen(string) + 1;
-    char* copy = NULL;
-    copy = malloc(size);
-    if(copy == NULL) return NULL;
-    memcpy(copy, string, size);
-    copy[strlen(string) + 1] = '\0';
-    return copy;
-}
 
 #define naettAlloc(TYPE, VAR) TYPE* VAR = (TYPE*)calloc(1, sizeof(TYPE))
 
